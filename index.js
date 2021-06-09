3b435549b92d707d7cbfc672f8b4a78629e2a08c
Lan Tian <xuyh0120@outlook.com>
2021-06-09 23:37:13 +0800

    Remove all default plugins

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
 'use strict';
 
-const def_pugs_list = [
-  'markdown-it-abbr',
-  'markdown-it-anchor',
-  'markdown-it-attrs',
-  'markdown-it-checkbox',
-  'markdown-it-deflist',
-  'markdown-it-emoji',
-  'markdown-it-footnote',
-  'markdown-it-ins',
-  'markdown-it-katex',
-  'markdown-it-mark',
-  'markdown-it-sub',
-  'markdown-it-sup'
-];
-const def_pugs_conf = {
-  'markdown-it-anchor': {
-    level: 1,
-    slugify: string => string,
-    permalink: true,
-    permalinkClass: 'header-anchor',
-    permalinkSymbol: '¶',
-    permalinkBefore: true
-  },
-  'markdown-it-checkbox': {
-    divWrap: false,
-    divClass: 'checkbox',
-    idPrefix: 'checkbox'
-  },
-  'markdown-it-katex': {
-    throwOnError: false,
-    errorColor: '#cc0000',
-    csslink: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css'
-  }
-};
 const def_main_conf = {
   html: true,
   xhtmlOut: false,
@@ -46,18 +12,14 @@ const def_main_conf = {
 let renderer = function (data, options) {
   let merge = require('lodash.merge');
   let keys = require('lodash.keys');
-  let union = require('lodash.union');
   let config = merge({}, this.config.markdown);
   let main_conf = merge({}, def_main_conf, config.render);
-  let pugs_conf = merge({}, def_pugs_conf, config.plugins);
-  let pugs_list = union(def_pugs_list, keys(pugs_conf));
+  let pugs_conf = config.plugins;
+  let pugs_list = keys(pugs_conf);
   let parser = require('markdown-it')(main_conf);
 
   parser = pugs_list.reduce(function (parser, pugs) {
     let pugs_opt = merge({}, pugs_conf[pugs]);
-    if (pugs_opt.disabled) {
-      return parser;
-    }
     for (let item in pugs_opt) {
       if (
         typeof pugs_opt[item] === 'string' &&
@@ -66,9 +28,6 @@ let renderer = function (data, options) {
       ) {
         pugs_opt[item] = eval('(' + pugs_opt[item] + ')');
       }
-    }
-    if (pugs === 'markdown-it-katex' && data.path) {
-      data.text += `\n\n<link rel="stylesheet" href="${pugs_opt.csslink}">`;
     }
     if (pugs_opt._parser) {
       return eval('(' + pugs_opt._parser + ')');
